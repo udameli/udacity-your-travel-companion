cbf332caec87129c5903c9c33842d6f210b1489d
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2017-07-06 13:04:47 +0600

    Add project files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Udacity Travel Companion Project
+
+This project was created as a part of the Udacity's Ajax course. The project implements both JQuery and Ajax. The purpose of this project is to demonstrate the use of JQuery and Ajax in the application. 
+
+### Managing the application
+
+After you downloaded the project files:
+
+* open index.html with any browser 
+* enter full street name, city and province into the form at the top of the page. See example below:
+	* Street: 301 Front St W
+	* City: Toronto, ON
+* the background of the page will turn into the streetview of the entered location 
+* list of all NYTimes articles that mention the city name will appear on the left 
+* list of all Wikipedia links related to the mentioned city will appear on the right 
+
+## Author
+
+* Dameli Ushbayeva
+
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -11,10 +10,57 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // Google Streetview request
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street+', '+city;
+    
+    $greeting.text('So, you want to live at ' + address + '?');
 
-    // YOUR CODE GOES HERE!
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/' + 
+                        'streetview?size=600x450&location=' + address; 
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    
+    // NYTimes Ajax request
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+                    city + '&sort=newest&api-key=2f65195b7b404009be295d918b36c931';
+    
+    $.getJSON(nytimesUrl, function (data) {
+        $nytHeaderElem.text('New York Times Articles About ' + city);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="' + 
+                            article.web_url+'">' + article.headline.main + 
+                            '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        }
+    }).fail(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')
+    });
 
+    // Wikipedia Ajax request
+    var wikipediaUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + 
+                        city + '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text('Failed To Get Wikipedia Resources');
+    }, 8000);
+
+    $.ajax({
+        url: wikipediaUrl,
+        dataType: 'jsonp',
+        success: function (response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+            
+            clearTimeout(wikiRequestTimeout);        
+        }
+    });
     return false;
 };
 
